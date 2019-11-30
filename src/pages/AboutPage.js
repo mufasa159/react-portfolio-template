bbd6d7fe2a5b528153ca72b004a59a76990c939c
Muhfasul Alam <muhfasulpushan159@gmail.com>
2019-11-29 20:51:43 -0500

    Update AboutPage.js

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -5,7 +5,7 @@ function AboutPage(){
     return(
         <div className="main" data-aos="fade-up">
             <h1>About Me</h1>
-            <p>I am currently working as an intern at a software company named <span>The Glass Files</span>. However, I'm always interested in new opportunities to learn more things and explore. </p>
+            <p>I am currently working as an intern at a software company named <span>The Glass Files</span>. However, I am always interested in new opportunities to learn more things and explore. </p>
         </div>
         
 
