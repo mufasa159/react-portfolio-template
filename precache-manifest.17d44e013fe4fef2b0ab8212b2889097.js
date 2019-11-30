4a92847d41b8e2facf35aa58459a064d44376497
Muhfasul Alam <muhfasulpushan159@gmail.com>
2019-11-29 22:01:34 -0500

    Updates

--- precache-manifest.17d44e013fe4fef2b0ab8212b2889097.js
+++ precache-manifest.17d44e013fe4fef2b0ab8212b2889097.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a59c41e6f61285c365c352b152911d6b",
+    "revision": "8b56284013337f78f63ee391ec465026",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.b0bb7f2c.chunk.css"
   },
   {
-    "revision": "00bd9c6533d64d6233c4",
+    "revision": "6a448aec46283ec1e2ee",
     "url": "/static/css/main.ce60ae7a.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.42f4c8cb.chunk.js"
   },
   {
-    "revision": "00bd9c6533d64d6233c4",
-    "url": "/static/js/main.26ce2023.chunk.js"
+    "revision": "6a448aec46283ec1e2ee",
+    "url": "/static/js/main.7a65e0f9.chunk.js"
   },
   {
     "revision": "0ac2b0fd5fd4af3be109",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3bd25431e1e02ce9a096743727268946.js"
+  "/precache-manifest.17d44e013fe4fef2b0ab8212b2889097.js"
 );
 
 self.addEventListener('message', (event) => {
