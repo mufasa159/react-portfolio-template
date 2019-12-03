e408d6c14def4584144a9e73056c103b52cb42a1
Muhfasul Alam <muhfasulpushan159@gmail.com>
2019-12-03 17:25:35 -0500

    grammar fixed

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -6,7 +6,7 @@ function AboutPage(){
         <div className="main" data-aos="fade-up">
             <h1>About Me</h1>
             <p>I am currently working as an intern at a software company named <span>The Glass Files</span>. However, I am always interested in new opportunities to learn more things and explore. </p>
-            <p>More things to added here. </p>
+            <p>More things to be added here. </p>
         </div>
         
 
